e428d081646a6eaa012b4edf66b0981456cad8c2
VTariko <vv.tariko@gmail.com>
2017-06-03 14:35:19 +0300

    Начало реализации змейки - создана форма, пустой класс для блоков

---
+++ Snake/SnakeBlock.cs
@@ -0,0 +1,32 @@
+﻿namespace Snake
+{
+	/// <summary>
+	/// Отдельный блок змеи
+	/// </summary>
+	class SnakeBlock
+	{
+		#region Поля
+
+
+
+		#endregion
+
+		#region Свойства
+
+
+
+		#endregion
+
+		#region Конструкторы
+
+
+
+		#endregion
+
+		#region Методы
+
+
+
+		#endregion
+	}
+}
